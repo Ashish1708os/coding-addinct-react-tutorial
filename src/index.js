c49f5df85043dd49cda5da1dd51b1443ce222f45
Ashish Salve <ashishsalve1777@gmail.com>
2021-11-04 20:54:02 +0530

    mimi book project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
 import React from "react";
 import ReactDom from "react-dom";
+// importing CSS
+import "./index.css";
 
-function Greeting() {
+//setup var
+const author = "Mark Manson";
+const title = "The Subtle Art of Not Giving a F*ck";
+const image =
+  "https://images-eu.ssl-images-amazon.com/images/I/814jC%2BrODgL._AC_UL200_SR200,200_.jpg";
+
+function BookList() {
   return (
-    <div>
-      <Person />
-    </div>
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
   );
 }
 
-const Person = () => <h2>Ashish Salve</h2>;
-const Message = () => {
-  return <p>this is my message</p>;
+const Book = () => {
+  return (
+    <article className="book">
+      <img src={image} alt="book" />
+      <h1>{title}</h1>
+      <h4
+        style={{
+          color: "#617d98",
+          fontSize: "0.75rem",
+          marginTop: "0.25rem",
+        }}
+      >
+        {author.toUpperCase()}
+      </h4>
+    </article>
+  );
 };
 
-ReactDom.render(<Greeting />, document.getElementById("root"));
+ReactDom.render(<BookList />, document.getElementById("root"));
